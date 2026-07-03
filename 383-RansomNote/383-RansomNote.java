20b1110b2cf088948c3b845dd01dc50e1c332cc1
Sabhariessh <sabhariessh@gmail.com>
2026-07-03 11:39:20 +0530

    Create 383-RansomNote

---
+++ 383-RansomNote/383-RansomNote.java
@@ -0,0 +1,19 @@
+// Last updated: 03/07/2026, 11:39:21
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> Hash = new HashMap<>();
+
+        for (char c : magazine.toCharArray()) {
+            Hash.put(c, Hash.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : ransomNote.toCharArray()) {
+            if (!Hash.containsKey(c) || Hash.get(c) <= 0) {
+                return false;
+            }
+            Hash.put(c, Hash.get(c) - 1);
+        }
+
+        return true;
+    }
+}
