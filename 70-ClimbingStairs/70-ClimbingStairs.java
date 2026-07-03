eb0aa655cdbc86fdd94a5cc95f7e3f3acaa7a63d
Sabhariessh <sabhariessh@gmail.com>
2026-07-03 11:40:38 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -0,0 +1,15 @@
+// Last updated: 03/07/2026, 11:40:39
+class Solution {
+    public int climbStairs(int n) {
+        if(n==1){
+            return 1;
+        }
+        int[] dp=new int[n+1];
+        dp[1]=1;
+        dp[0]=1;
+       for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+}
