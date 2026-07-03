6063b5a66ebc56117cecf5b72de3288e03de0dc8
Sabhariessh <sabhariessh@gmail.com>
2026-07-03 11:38:40 +0530

    Create 747-MinCostClimbingStairs

---
+++ 747-MinCostClimbingStairs/747-MinCostClimbingStairs.java
@@ -0,0 +1,13 @@
+// Last updated: 03/07/2026, 11:38:41
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n=cost.length;
+        int[] dp=new int[n];
+        dp[0]=cost[0];
+        dp[1]=cost[1];
+        for(int i=2;i<n;i++){
+            dp[i]=Math.min(dp[i-1],dp[i-2])+cost[i];
+        }
+        return Math.min(dp[n-2],dp[n-1]);
+    }
+}
