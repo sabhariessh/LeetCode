72419ffe7b48f028c426312899ed51117a882461
Sabhariessh <sabhariessh@gmail.com>
2026-07-15 12:01:36 +0530

    Create 3228-MaximumSizeOfASetAfterRemovals

---
+++ 3228-MaximumSizeOfASetAfterRemovals/3228-MaximumSizeOfASetAfterRemovals.java
@@ -0,0 +1,31 @@
+// Last updated: 15/07/2026, 12:01:35
+class Solution {
+    public int maximumSetSize(int[] nums1, int[] nums2) {
+        Set<Integer> s1 = new HashSet<>();
+        Set<Integer> s2 = new HashSet<>();
+        int a=0,b=0,c=0;
+        for (int x : nums1){
+            s1.add(x);
+        }
+        for (int x : nums2){
+            s2.add(x);
+        }
+        for (int x : s1) {
+            if (!s2.contains(x)){
+                a++;
+            }
+        }
+        for (int x : s2) {
+            if (!s1.contains(x)){
+                b++;
+            }
+            else{
+                c++;
+            }
+        }
+        int n = nums1.length;
+        a = Math.min(a, n / 2);
+        b = Math.min(b, n / 2);
+        return Math.min(a + b + c, n);
+    }
+}
