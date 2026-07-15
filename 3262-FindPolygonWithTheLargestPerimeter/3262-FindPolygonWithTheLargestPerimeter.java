44529054c69afe7287559f599b25e574a206db8b
Sabhariessh <sabhariessh@gmail.com>
2026-07-15 12:01:33 +0530

    Create 3262-FindPolygonWithTheLargestPerimeter

---
+++ 3262-FindPolygonWithTheLargestPerimeter/3262-FindPolygonWithTheLargestPerimeter.java
@@ -0,0 +1,15 @@
+// Last updated: 15/07/2026, 12:01:32
+class Solution {
+    public long largestPerimeter(int[] nums) {
+        Arrays.sort(nums);
+        long sum = 0;
+        long ans = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < sum) {
+                ans = nums[i] + sum;
+            }
+            sum += nums[i];
+        }
+        return ans;
+    }
+}
