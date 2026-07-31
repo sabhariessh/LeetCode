e2bf8a919cadd6e32f622f00b2d053c259736a55
Sabhariessh <sabhariessh@gmail.com>
2026-07-31 09:26:39 +0530

    Create 3859-MaximumProductOfTwoDigits

---
+++ 3859-MaximumProductOfTwoDigits/3859-MaximumProductOfTwoDigits.java
@@ -0,0 +1,18 @@
+// Last updated: 31/07/2026, 09:26:39
+class Solution {
+    public int maxProduct(int n) {
+        int max1 = -1 , max2 = -1;
+        while(n != 0){
+            int rem = n % 10;
+            if(max1 <= rem){
+                max2 = max1;
+                max1 = rem;
+            }
+            else if(max2 < rem){
+                max2 = rem;
+            }
+            n /= 10;
+        }
+        return max1 * max2;
+    }
+}
